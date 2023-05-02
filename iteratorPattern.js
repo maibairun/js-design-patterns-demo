b2c7f20eddbcbb39f300720578ccbbbc27438981
PakJeon <makpakjeon@gmail.com>
2023-05-02 22:34:16 +0800

    feat: 迭代器模式

--- iteratorPattern.js
+++ iteratorPattern.js
@@ -1,9 +1,10 @@
 /*
- * :file description: 
+ * :file description: 迭代器模式
  * :name: \js-design-patterns-demo\iteratorPattern.js
  * :author: PakJeon
  * :copyright: (c) 2023, Tungee
  * :date created: 2023-05-02 22:02:36
  * :last editor: PakJeon
- * :date last edited: 2023-05-02 22:02:41
+ * :date last edited: 2023-05-02 22:34:10
  */
+// 迭代器模式是一种相对简单的模式，简单到很多时候我们都不认为它是一种设计模式。目前的绝大部分语言都内置了迭代器。
